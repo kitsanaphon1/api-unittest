1e1d2c94c6679c1aa106b864ed6f99afe96d05d7
kitsanaphon1 <123629506+kitsanaphon1@users.noreply.github.com>
2025-06-30 09:15:54 +0700

    commit

--- WebApiProject/obj/Debug/net9.0/WebApiProject.AssemblyInfo.cs
+++ WebApiProject/obj/Debug/net9.0/WebApiProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApiProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c5639873a7f0e77978171077e725022d47853e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApiProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApiProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
